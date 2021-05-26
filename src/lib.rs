833f57d0c6c7f2d021ac686dc04cbd7807cda253
Matthew Blanchard <matthewblanc@msn.com>
2021-05-26 17:00:47 -0400

    Added support for images that are not the default font atlas.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,45 @@
 use imgui as imgui_rs;
 use imgui_rs::{Context, DrawData};
-use skia_safe::Matrix;
+use skia_safe::{AlphaType, Matrix, Paint};
+use std::collections::HashMap;
 
 pub struct Renderer {
     // this holds the skia formatted font atlas
-    skfont_paint: skia_safe::Paint,
+    images: HashMap<usize, skia_safe::Paint>,
+    img_idx: usize // this is incremented each time an image is registered and is the id returned to the caller
 }
 
 impl Renderer {
+    pub fn load_image_rgba8(img: &[u8], width: i32, height: i32) -> skia_safe::Paint {
+        let mut paint = Paint::default();
+        let dimensions = skia_safe::ISize::new(width, height);
+        let img_info_rgba8 = skia_safe::ImageInfo::new_n32(dimensions, AlphaType::Unknown, None);
+
+        let pixels = unsafe {
+            skia_safe::Data::new_bytes(img)
+        };
+
+        let pixmap = skia_safe::Pixmap::new(&img_info_rgba8, img, img_info_rgba8.min_row_bytes());
+        let image = skia_safe::Image::from_raster_data(&img_info_rgba8, pixels, pixmap.row_bytes());
+
+        let local_matrix = skia_safe::Matrix::scale((1.0 / width as f32, 1.0 / height as f32));
+        let sampling_options = skia_safe::SamplingOptions::new(skia_safe::FilterMode::Nearest, skia_safe::MipmapMode::None);
+        let tile_mode = skia_safe::TileMode::Repeat;
+
+        let image_shader = image.unwrap().to_shader((tile_mode, tile_mode), sampling_options, &local_matrix);
+
+        paint.set_shader(image_shader);
+        paint.set_color(skia_safe::Color::WHITE);
+
+        return paint;
+    }
+
+    pub fn register_image(&mut self, paint: skia_safe::Paint) -> usize{
+        self.images.insert(self.img_idx, paint);
+        self.img_idx += 1;
+        return self.img_idx - 1;
+    }
+
     fn build_paint(atlas: &mut imgui::FontAtlasRefMut, font_paint: &mut skia_safe::Paint)
     {
         let imfont_texture = atlas.build_alpha8_texture();
@@ -33,12 +65,16 @@ impl Renderer {
 
     pub fn new(im_context: &mut Context) -> Self
     {
+        let mut ret = Renderer {
+            images: HashMap::new(),
+            img_idx: 0,
+        };
+
         let mut font_paint = skia_safe::Paint::default();
         Self::build_paint(&mut im_context.fonts(), &mut font_paint);
-    
-        Renderer {
-            skfont_paint: font_paint,
-        }
+        ret.register_image(font_paint);
+
+        ret
     }
 
     pub fn render_imgui(&self, canvas: &mut skia_safe::Canvas, data: &DrawData, )
@@ -97,7 +133,8 @@ impl Renderer {
                         cmd_params,
                     } => {
                         //TODO: Handle images that aren't our font atlas
-                        //let id_index = cmd_params.texture_id;
+                        let id_index = cmd_params.texture_id;
+                        let paint = &self.images[&id_index.id()];
 
                         let clip_rect = cmd_params.clip_rect;
                         let skclip_rect = skia_safe::Rect::new(clip_rect[0], clip_rect[1], clip_rect[2], clip_rect[3]);
@@ -108,7 +145,7 @@ impl Renderer {
 
                         arc.clip_rect(skclip_rect, skia_safe::ClipOp::default(), true);
                         let vertices = skia_safe::Vertices::new_copy(vertex_mode, &pos, &uv, &color, idx_slice);
-                        arc.draw_vertices(&vertices, skia_safe::BlendMode::Modulate, &self.skfont_paint);
+                        arc.draw_vertices(&vertices, skia_safe::BlendMode::Modulate, paint);
                     }
                 }
             }
